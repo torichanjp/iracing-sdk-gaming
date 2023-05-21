eac357156528faaa9365df013fd72d7a6885d2c7
Shigeki TORII <torii.shigeki@dropsign.in>
2023-05-21 13:44:24 +0900

    CI/CD

---
+++ bin/build_simple.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+set -eu
+
+VERSION=$1
+
+BIN_DIR="$(dirname $0)"
+
+BASE_DIR="$(cd ${BIN_DIR}/..; pwd)"
+echo "BASE_DIR: $BASE_DIR"
+
+BUILD_DIR="$BASE_DIR/build"
+SRC_DIR="$BASE_DIR/plugins"
+
+# buildディレクトリ削除
+echo "Deleting a build directory $BUILD_DIR..."
+
+if [[ -d "${BUILD_DIR}" ]]
+then
+    rm -rf "${BUILD_DIR}"
+fi
+
+echo "Done"
+echo
+
+
+#
+# plugins以下をbuildにコピー
+#
+echo "Copyng files in $SRC_DIR to $BUILD_DIR..."
+cp -a ${SRC_DIR} ${BUILD_DIR}
+echo "Done"
+
+ls -R $BUILD_DIR
+
+cd "${BUILD_DIR}"
+
+# MarkDownからPDFを作成
+echo "Generating PDF file(s) from MarkDown..."
+
+export LANG=ja_JP.utf-8
+
+# ./**/*.mdがなぜかgithub環境だと動かないので1ファイルずつ出力する
+for i in $(find . -name '*.md'); do
+  echo "Target file: ${i}..."
+  npx md-to-pdf $i
+  if [[ $? != 0 ]]; then
+      echo "Failed to make PDF file(s)" 2>&1
+      exit 1
+  fi
+  echo "Finished"
+done
+
+echo "Done"
+echo
+
+# 元のmdファイルを削除
+echo "Delete markdown file(s)..."
+find ${BUILD_DIR} -name '*.md' -exec rm {} \;
+
+echo "Done"
+echo
+echo "Packaging..."
+zip -r ${BUILD_DIR}/sdk-gaming-plugins-by-torichanjp-${VERSION}.zip .
+if [[ $? != 0 ]];then
+    "Fail" 2>&1
+    exit 1
+fi
+echo "Success"
+
+exit 0
